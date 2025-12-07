ec58fe0376ef4b99379c5bdd3273207fa3848942
kevinmutai <mutaikevinkip.05@gmail.com>
2025-12-07 13:28:14 +0300

    page setup plus event cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,27 @@
+import EventCard from "@/components/EventCard"
+import ExploreBtn from "@/components/ExploreBtn"
+import { events } from "@/lib/constants"
 
 
 const page = () => {
   return (
-    <div>
-      <h1>Welcome to the Events App</h1>
-    </div>
+    <section>
+      <h1 className="text-center">The Hub for Every Dev <br/> Event You Mustn't Miss</h1>
+      <p className="text-center mt-5">Hackathons, Meetups, and Conferences, All in One Place</p>
+      <ExploreBtn/>
+      <div className="mt-20 space-y-7">
+        <h3>Featured Events</h3>
+        <ul className="events list-none">
+          {
+            events.map((event) => (
+              <li key={event.title}>
+                <EventCard {...event}/>
+              </li>
+            ))
+          }
+        </ul>
+      </div>
+    </section>
   )
 }
 
