3a2d15029230f1c25846b7e389893d9b43f83aa7
kevinmutai <mutaikevinkip.05@gmail.com>
2025-12-16 13:45:12 +0300

    return previous

--- app/page.tsx
+++ app/page.tsx
@@ -2,19 +2,18 @@ import EventCard from "@/components/EventCard"
 import ExploreBtn from "@/components/ExploreBtn"
 import { IEvent } from "@/database"
 import { cacheLife } from "next/cache"
-import { events } from "@/lib/constants"
 
 
-//const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL
 
 if (!process.env.BASE_URL) console.warn('NEXT_PUBLIC_URL is not set')
 
 const page = async () => {
   'use cache'
   cacheLife('hours')
-  //const response = await fetch(`${BASE_URL}/api/events`)
+  const response = await fetch(`${BASE_URL}/api/events`)
   
-  //const { events } = await response.json()
+  const { events } = await response.json()
 
   return (
     <section>
